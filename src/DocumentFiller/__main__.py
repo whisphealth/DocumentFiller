005bcbfeeccc8b9c0b6f18f7a79aa71fddfb2792
Mathieubola <mathieulassurguere@hotmail.fr>
2021-10-22 12:32:29 +0300

    Changed the way the cli work
    Changed the way a user use the library using cli. This way allow the user to use the wildcard for the input files and allow to not specify the output file.

--- src/DocumentFiller/__main__.py
+++ src/DocumentFiller/__main__.py
@@ -1,64 +1,60 @@
 from argparse import ArgumentParser
 from genericpath import isdir, isfile
 from json import load, loads
-from posix import listdir
-from re import search
+from re import search, sub
 from .DocFillerFactory import DocumentFillerFactory
 
 
-def parse_path(path_in: str, path_out: str) -> zip:
+def parse_path(path_in: list, path_out: str) -> zip:
     """
-    Take the two path to a folder or a file and parse them to a list of
-    paths to files
-    :param path_in: (String) Path to the input file or folder
+    Take the list of input files path and infos on the output
+    file and parse them to two list of input and output files
+    :param path_in: (List) List of path to the input files
     :param path_out: (String) Path to the output file or folder
     """
-    path_in = path_in + (
-        "/" if (path_in[-1:] != "/" and isdir(path_in)) else ""
-    )
-    path_out = path_out + (
-        "/" if (path_out[-1:] != "/" and isdir(path_out)) else ""
-    )
 
-    if isfile(path_in):
-        r_path_in = [path_in]
-    elif isdir(path_in):
-        r_path_in = [
-            path_in + f
-            for f in listdir(path_in)
-            if (isfile(path_in + f) and f[:2] != "~$")
+    if not path_out:
+        r_path_out = [sub(r"\.(?=[^\.\/]+?$)", ".out.", f) for f in path_in]
+    elif isdir(path_out):
+        path_out = path_out + ("/" if (path_out[-1:] != "/") else "")
+
+        r_path_out = [
+            path_out + search(r"[^\/]+?\..+?$", f).group(0) for f in path_in
         ]
-    else:
-        raise Exception("srcpath is not a file / directory")
-
-    if isfile(path_in) and not isdir(path_out):
+    elif isfile(path_out) and len(path_in) == 1:
         r_path_out = [path_out]
-    elif isfile(path_in) and isdir(path_out):
-        r_path_out = [path_out + search(r"[^\/]+?\..+?$", path_in).group(0)]
-    elif isdir(path_in) and isdir(path_out):
-        r_path_out = [f.replace(path_in, path_out) for f in r_path_in]
     else:
         raise Exception("srcpath and / or destpath is invalid")
 
-    return zip(r_path_in, r_path_out)
+    return zip(path_in, r_path_out)
 
 
 def main():
     parser = ArgumentParser(
         description="Fill documents by replacing flags in docx files and put"
         " the result in an other directory",
-        usage="DocumentFiller [-d|--debug] [--pdf] [--pdfonly] scrpath "
-        "jsonpath destpath",
+        usage="DocumentFiller [-d|--debug] [--pdf] [--pdfonly] "
+        "-j|-json|-jsonpath jsonpath [-o|-output outputpath] inputfile(s)",
     )
 
     parser.add_argument(
-        "srcpath", type=str, help="Source path to a docx file or folder"
+        "input",
+        type=str,
+        nargs="*",
+        help="Source path to a docx file(s)",
     )
     parser.add_argument(
-        "jsonpath", type=str, help="Json file path containing the keys"
+        "-j",
+        "--json",
+        "--jsonpath",
+        type=str,
+        help="Json file path containing the keys",
     )
     parser.add_argument(
-        "destpath", type=str, help="Destination path to a docx file or folder"
+        "-o",
+        "--output",
+        type=str,
+        help="Destination path to a folder or a docx file",
     )
     parser.add_argument(
         "-d", "--debug", action="store_true", help="Show debug infos"
@@ -77,32 +73,27 @@ def main():
     args = parser.parse_args()
 
     # Arguments errors detection
-    if (not isfile(args.srcpath)) and (not isdir(args.srcpath)):
-        raise Exception("First argument file/folder does not exist")
-    if not isfile(args.jsonpath):
+    if not all([isfile(f) for f in args.input]):
+        raise Exception("One or more input file does not exist")
+
+    if not isfile(args.json):
         try:
-            json = loads(args.jsonpath)
+            json = loads(args.json)
         except Exception:
             raise Exception(
                 "Second argument is not a file and is not parsable"
             )
     else:
         try:
-            json = load(open(args.jsonpath))
+            json = load(open(args.json))
         except Exception:
             raise Exception("Json file is not parsable")
 
-    # File path correction
-    file_dest_path = args.destpath + (
-        "/" if (args.destpath[-1:] != "/" and isdir(args.destpath)) else ""
-    )
-    file_src_path = args.srcpath + (
-        "/" if (args.srcpath[-1:] != "/" and isdir(args.srcpath)) else ""
-    )
+    # DocFiller
 
     dff = DocumentFillerFactory(args.debug, args.pdf, args.pdfonly)
     # Calling the function
-    for file_in, file_out in parse_path(file_src_path, file_dest_path):
+    for file_in, file_out in parse_path(args.input, args.output):
         doc_filler = dff.inst(search(r"[^\.]+?$", file_in).group(0))
         if doc_filler:
             doc_filler.fill(file_in, json, file_out)
